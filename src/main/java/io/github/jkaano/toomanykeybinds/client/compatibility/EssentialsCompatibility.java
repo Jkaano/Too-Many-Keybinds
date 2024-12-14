7ca5fe2c6a722f5eb1c3af31a97562bf1c032474
Jkaano <jkaano@gmail.com>
2024-12-14 12:46:48 -0600

    Put in comments until safe to remove

--- src/main/java/io/github/jkaano/toomanykeybinds/client/compatibility/EssentialsCompatibility.java
+++ src/main/java/io/github/jkaano/toomanykeybinds/client/compatibility/EssentialsCompatibility.java
@@ -20,13 +20,13 @@ public class EssentialsCompatibility {
 
         //If essentials is installed, turn off auto key press, or if it is uninstalled turn it on
         //Only do this if the user hasn't manually changed the key bind
-        if(essentials && !ClientConfig.LOCK_AUTO.get()){
-            ClientConfig.AUTOMATIC_KEY_PRESS.set(false);
-            ClientConfig.AUTOMATIC_KEY_PRESS.save();
-        }else if(!ClientConfig.LOCK_AUTO.get()){
-            ClientConfig.AUTOMATIC_KEY_PRESS.set(true);
-            ClientConfig.AUTOMATIC_KEY_PRESS.save();
-        }
+//        if(essentials && !ClientConfig.LOCK_AUTO.get()){
+//            ClientConfig.AUTOMATIC_KEY_PRESS.set(false);
+//            ClientConfig.AUTOMATIC_KEY_PRESS.save();
+//        }else if(!ClientConfig.LOCK_AUTO.get()){
+//            ClientConfig.AUTOMATIC_KEY_PRESS.set(true);
+//            ClientConfig.AUTOMATIC_KEY_PRESS.save();
+//        }
     }
 
 }
