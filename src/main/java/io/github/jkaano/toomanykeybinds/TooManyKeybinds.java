3188f8aef006c0afbf13a43ebd7cf8af442a5146
Jkaano <jkaano@gmail.com>
2024-12-22 22:01:22 -0600

    Remove unused code, register config screen to forge

--- src/main/java/io/github/jkaano/toomanykeybinds/TooManyKeybinds.java
+++ src/main/java/io/github/jkaano/toomanykeybinds/TooManyKeybinds.java
@@ -1,57 +1,22 @@
 package io.github.jkaano.toomanykeybinds;
 
-import io.github.jkaano.toomanykeybinds.client.KeyHandler;
-import io.github.jkaano.toomanykeybinds.client.TMKPageHandler;
-import io.github.jkaano.toomanykeybinds.client.compatibility.RobotCompatibilityChecker;
 import io.github.jkaano.toomanykeybinds.client.config.ClientConfig;
-import io.github.jkaano.toomanykeybinds.client.gui.ButtonIdentity;
+import io.github.jkaano.toomanykeybinds.client.pages.PageFileHandler;
+import io.github.jkaano.toomanykeybinds.client.screen.ConfigScreen;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Mod(TooManyKeybinds.MODID)
 public class TooManyKeybinds {
     public static final String MODID = "toomanykeybinds";
 
-    private static int page = 0;
-    private static int pageSelect = 0;
-
-    public static KeyHandler tmkHandler;
-    public static TMKPageHandler pageHandler;
-
-    public static boolean searching = false;
-
-    public static List<ButtonIdentity> keyQueue = new ArrayList<>();
-
-    public static RobotCompatibilityChecker robotCompatibilityChecker;
+    public static PageFileHandler pageFileHandler;
 
     public TooManyKeybinds(){
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ClientConfig.SPEC);
-        //Create compatibility checking object
-        robotCompatibilityChecker = new RobotCompatibilityChecker();
-    }
-
-    public static void setPage(int page) {
-        TooManyKeybinds.page = page;
-    }
-
-    public static void setPageSelect(int pageSelect){
-        TooManyKeybinds.pageSelect = pageSelect;
-    }
-
-    public static int getPage(){
-        return page;
-    }
-
-    public static int getPageSelect(){
-        return pageSelect;
-    }
-
-    public static RobotCompatibilityChecker getRobotCompatibilityChecker(){
-        return robotCompatibilityChecker;
+        MinecraftForge.registerConfigScreen(ConfigScreen::new);
     }
 
 }
