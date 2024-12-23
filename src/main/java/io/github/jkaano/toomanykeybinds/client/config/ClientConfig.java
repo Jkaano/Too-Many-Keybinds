682d5ec7ee7cd6cc00b3930fa4c46b1b839930a8
Jkaano <jkaano@gmail.com>
2024-12-22 21:51:32 -0600

    Removed PAGES config and added PRESS_DELAY

--- src/main/java/io/github/jkaano/toomanykeybinds/client/config/ClientConfig.java
+++ src/main/java/io/github/jkaano/toomanykeybinds/client/config/ClientConfig.java
@@ -7,19 +7,22 @@ public class ClientConfig {
     public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
     public static final ForgeConfigSpec SPEC;
 
-    //Automatic key press will
+    //Automatic key press will auto set on start up unless lock auto is set to true
     public static ForgeConfigSpec.ConfigValue<Boolean> AUTOMATIC_KEY_PRESS;
     public static ForgeConfigSpec.ConfigValue<Boolean> LOCK_AUTO;
 
-    public static ForgeConfigSpec.ConfigValue<String> PAGES;
+    public static ForgeConfigSpec.ConfigValue<Long> PRESS_DELAY;
+
+    public static final String AUTO_PRESS = "auto_press.";
 
     static {
 
         BUILDER.push("Too Many Keybinds Configuration");
 
-        AUTOMATIC_KEY_PRESS = BUILDER.comment("Automatic Key Press").define("auto_key_press", true);
-        LOCK_AUTO = BUILDER.define("lock_value_on_startup", false);
-        PAGES = BUILDER.comment("Pages - DO NOT TOUCH - INCOMPLETE").define("pages", ""); //Remember to change page comment if you fix this
+        AUTOMATIC_KEY_PRESS = BUILDER.comment("Automatic Key Press or User Requested Key Press").define(AUTO_PRESS + "auto_key_press", true);
+        LOCK_AUTO = BUILDER.define(AUTO_PRESS + "lock_value_on_startup", false);
+
+        PRESS_DELAY = BUILDER.comment("Cancel Delay(Seconds) - Applies if auto key press is off").defineInRange("cancel_delay", 5L, 1L, 60L);
 
         BUILDER.pop();
         SPEC = BUILDER.build();
