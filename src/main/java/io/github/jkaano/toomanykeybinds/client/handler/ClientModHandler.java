cc0aa4589261e458fe82918dbbc0ad664bd910b2
Jkaano <jkaano@gmail.com>
2024-12-22 21:52:59 -0600

    Removed unneeded code, added file handler initialization to client setup

--- src/main/java/io/github/jkaano/toomanykeybinds/client/handler/ClientModHandler.java
+++ src/main/java/io/github/jkaano/toomanykeybinds/client/handler/ClientModHandler.java
@@ -2,8 +2,8 @@ package io.github.jkaano.toomanykeybinds.client.handler;
 
 import io.github.jkaano.toomanykeybinds.TooManyKeybinds;
 import io.github.jkaano.toomanykeybinds.client.Keybindings;
-import io.github.jkaano.toomanykeybinds.client.KeyHandler;
-import io.github.jkaano.toomanykeybinds.client.compatibility.EssentialsCompatibility;
+import io.github.jkaano.toomanykeybinds.client.pages.PageFileHandler;
+import io.github.jkaano.toomanykeybinds.client.pages.PageHandler;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -13,25 +13,22 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 @Mod.EventBusSubscriber(modid = TooManyKeybinds.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientModHandler {
 
+    public static PageHandler pageHandler;
+
     @SubscribeEvent
     public static void clientSetup(FMLClientSetupEvent event){
-
-        //Set compatibilities
-        EssentialsCompatibility.setEssentials();
 
         //Create an initial configuration support for the KeyHandler to use as it defines some variables
         RobotHandler.setCompatConfig(); //Maybe move key press handling to a new class so I don't have to initialize and then reset later?
 
-        System.out.println("Too Many Keybinds: Creating keybind list");
-        TooManyKeybinds.tmkHandler = new KeyHandler();
-        System.out.println("Too Many Keybinds: Successfully created keybind list");
+        TooManyKeybinds.pageFileHandler = new PageFileHandler();
+        pageHandler = new PageHandler();
 
     }
 
     @SubscribeEvent
     public static void registerKeys(RegisterKeyMappingsEvent event){
         event.register(Keybindings.INSTANCE.openTMK);
-        event.register(Keybindings.INSTANCE.keySetter);
     }
 
 }
