88da51247ec6dab555fd9bfe1c37807abffe181b
Jkaano <jkaano@gmail.com>
2025-01-12 16:28:25 -0600

    Added better logger

--- src/main/java/io/github/jkaano/toomanykeybinds/TooManyKeybinds.java
+++ src/main/java/io/github/jkaano/toomanykeybinds/TooManyKeybinds.java
@@ -7,10 +7,13 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(TooManyKeybinds.MODID)
 public class TooManyKeybinds {
     public static final String MODID = "toomanykeybinds";
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
 
     public static PageFileHandler pageFileHandler;
 
